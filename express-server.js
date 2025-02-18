3c0d64cab1728cd43652750e1203f51c89a3d821
aathi-aathi <aathivdr2004@gmail.com>
2025-02-18 20:33:24 +0530

    commit-1

--- express-server.js
+++ express-server.js
@@ -4,15 +4,6 @@ import cors from 'cors'
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use((req,res,next)=>{
-    console.log(req.method, req.url)
-    if(req.method == 'PUT'){
-        next()
-    }else{
-        res.status(404).send({msg:'method is invalid'})
-    }
-    
-})
 app.use('/send-mail',mailRouter)
 const port = 8453
 app.listen(port,()=>{
